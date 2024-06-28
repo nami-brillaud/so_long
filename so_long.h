292017209459e5fd26a838d4819a327cbcfeb664
Nami Fujisaki <nfujisak@c2r2s8.42tokyo.jp>
2024-06-28 18:18:44 +0900

    header

---
+++ so_long.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfujisak <nfujisak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 16:29:12 by nfujisak          #+#    #+#             */
+/*   Updated: 2024/06/28 17:24:16 by nfujisak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "./libft/libft.h"
+# include "./printf/ft_printf.h"
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+#endif
